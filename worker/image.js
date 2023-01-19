be88a292a73853a68b7f12970bd41a5e90d187f4
Andrzej Surdej <Andrzej_Surdej@comcast.com>
2023-01-19 14:23:59 +0100

    worker

---
+++ worker/image.js
@@ -0,0 +1,8 @@
+ // Base64 representation of a white point image
+ const webpdata = 'data:image/webp;base64,UklGRiQAAABXRUJQVlA4IBgAAAAwAQCdASoCAAEAAQAcJaQAA3AA/v3AgAA=';
+ var image
+
+ // Retrieve the Image in Blob Format
+ fetch(webpdata).then(response => response.blob()).then((blob) => {
+  image = createImageBitmap(blob);
+ });
